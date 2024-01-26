f11290f175d2ddd69fea87126ca34a3250b00f09
Demessie-12 <hemenjkoo@gmail.com>
2024-01-26 16:13:55 +0300

    16.12

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Error from "./Error"
 import StartScreen from "./startScreen"
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -13,7 +15,8 @@ const initialState = {
   status: "loading",
   questionIndex: 0,
   answer: null,
-  points: 0
+  points: 0,
+  highscore: 0
 };
 
 function reducer(state, action) {
@@ -28,18 +31,25 @@ function reducer(state, action) {
       const question = state.questions.at(state.questionIndex)
       return {...state,
         answer: action.payload,
-        points: action.payload === question.correctAnswer ? state.points + question.points : state.points
+        points: action.payload === question.correctOption ? state.points + question.points : state.points
         }
     case "nextQuestion":
       return {...state, answer: null, questionIndex: state.questionIndex + 1}
+    case "finish":
+      return {...state, answer: null, status: "finished", highscore: state.points > state.highscore ? state.points : state.highscore}
+    case "restart":
+      return {...initialState, questions: state.questions, status: "ready"}
     default:
       throw new Error("Action unkown")
   }
 }
 
 export default function App () {
-  const [{questions, status, questionIndex, answer}, dispatch] = useReducer(reducer, initialState)
+  const [{questions, status, questionIndex, answer, points, highscore}, dispatch] = useReducer(reducer, initialState)
+
   const numQuestions = questions.length
+  const maxPossiblePoints = questions.reduce((prev, cur) =>
+    prev + cur.points, 0)
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -55,10 +65,12 @@ export default function App () {
       {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/> }
       {status === "active" && (
         <>
+        <Progress index={questionIndex} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}/>
         <Question question={questions[questionIndex]} dispatch={dispatch} answer={answer}/>
-        <NextButton dispatch={dispatch} answer={answer}/>
+        <NextButton dispatch={dispatch} answer={answer} questionIndex={questionIndex} numQuestions={numQuestions}/>
         </>
       )}
+      {status === "finished" && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore} dispatch={dispatch}/>}
     </Main>
   </div>
 }
--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function NextButton({dispatch, answer}) {
-    if(answer === null) return null
-  return (
+function NextButton({dispatch, answer, numQuestions, questionIndex}) {
+
+  if(answer === null) return null
+
+  if(questionIndex < numQuestions - 1) return (
     <button className="btn btn-ui" onClick={()=> dispatch({type:"nextQuestion"})}>Next</button>
+  )
+  if(questionIndex === numQuestions - 1) return (
+    <button className="btn btn-ui" onClick={()=> dispatch({type:"finish"})}>Finish</button>
   )
 }
 
